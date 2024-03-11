04e4bc6ab8ee774d287e03c9bb5afbe9baa1d626
Richard Cardone <rcardone@tacc.utexas.edu>
2024-03-11 17:22:55 -0500

    public key api using get

--- src/v1/tms/public_key.rs
+++ src/v1/tms/public_key.rs
@@ -1,4 +1,4 @@
-use poem_openapi::{ OpenApi, payload::Json, Object };
+use poem_openapi::{ param::Query, OpenApi, payload::Json, Object };
 use poem::Error;
 
 // ***************************************************************************
@@ -27,9 +27,15 @@ struct RespPublicKey
 // ***************************************************************************
 #[OpenApi]
 impl PublicKeyApi {
-    #[oai(path = "/tms/PublicKey", method = "post")]
-    async fn get_new_ssh_keys(&self, keys: Json<ReqPublicKey>) -> Json<RespPublicKey> {
-        let resp = match RespPublicKey::process(&keys) {
+    #[oai(path = "/tms/PublicKey", method = "get")]
+    async fn get_new_ssh_keys(&self, client_addr: Query<String>,
+                              client_port: Query<i32>,
+                              server_addr: Query<String>,
+                              server_port: Query<i32>,
+                              user: Query<String>,
+                             ) -> Json<RespPublicKey> {
+        let req = ReqPublicKey::new(client_addr.0, client_port.0, server_addr.0, server_port.0, user.0);                      
+        let resp = match RespPublicKey::process(&req) {
             Ok(r) => r,
             Err(e) => RespPublicKey::new("ERROR"),
         };
@@ -41,6 +47,14 @@ impl PublicKeyApi {
 // ***************************************************************************
 //                          Request/Response Methods
 // ***************************************************************************
+impl ReqPublicKey {
+    fn new(client_addr: String, client_port: i32, server_addr: String, 
+           server_port: i32, user: String) -> ReqPublicKey {
+                ReqPublicKey {client_addr, client_port, server_addr, server_port, user}
+           }
+}
+
+
 impl RespPublicKey {
     fn new(key: &str) -> Self {
         Self {public_key: key.to_string()}
