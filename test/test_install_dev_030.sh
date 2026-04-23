a8ec66da7dab5be0721a11b7c9a6b2ec48aaa45a
Steve Black <scblack@tacc.utexas.edu>
2026-04-23 15:56:32 -0500

    0.3.0 WIP Iinstall/upgrade script. Update for DEV testing.

--- test/test_install_dev_030.sh
+++ test/test_install_dev_030.sh
@@ -3,6 +3,8 @@
 # Script to test clean install of 0.3.0 in the DEV environment, tms-server-dev.tacc.utexas.edu
 #  - remove previous install from ~tms/.tms
 #  - setup env variables for install
+#  - reset the DB
+#  - run the install
 #  - as final check run tms_server to get the version
 #
 # Determine absolute path to location from which we are running and change to that directory.
@@ -25,15 +27,17 @@ fi
 echo "**********************************************************************"
 echo "   Removing previous installation"
 echo "**********************************************************************"
+# For DEV test use standard tms home dir of ~tms/.tms
+# Use hard-coded paths to avoid mistakes
 rm -fr ~tms/.tms
 rm -fr /opt/tms_server/lib
 rm /opt/tms_server/tms_server
 rm /opt/tms_server/tms.version
+export TMS_INSTALL_DIR=/opt/tms_server
+export TMS_ROOT_DIR=~tms/.tms
 
 # Set up env variables for running install
-. $SRC_DIR/test_install_dev.env
-
-# TODO ?????????????????????????????????????????/
+. $PRG_PATH/test_install_dev.env
 
 # Reset the postgres DB
 echo "**********************************************************************"
@@ -64,14 +68,11 @@ fi
 # TMS_LOCAL_DIR used for install output and custom tms.toml, log4rs.yml files.
 mkdir -p $LOCAL_DIR
 chmod 700 $LOCAL_DIR
-cp -p $SRC_DIR/test/tms_test_local.toml $LOCAL_DIR/tms.toml
+cp -p $SRC_DIR/test/tms_test_dev.toml $LOCAL_DIR/tms.toml
 chmod 600 $LOCAL_DIR/tms.toml
-cp -p $SRC_DIR/test/tms_test_cert.path $LOCAL_DIR/cert.path
-cp -p $SRC_DIR/test/tms_test_cert.path $LOCAL_DIR/key.path
-chmod 600 $LOCAL_DIR/*.path
 
 # Run the install
-$SRC_DIR/deployment/native/install_030.sh --test
+$SRC_DIR/deployment/native/install_030.sh
 RET_CODE=$?
 if [ $RET_CODE -ne 0 ]; then
   echo "Install of tms_server failed."
