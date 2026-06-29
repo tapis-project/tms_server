44bb6c8ce4502c5cef0eb8aa7de0d7557d3198eb
Walter Moreira <walter@waltermoreira.net>
2026-06-29 12:22:00 -0500

    Add examples to config.nix

--- nix/config.nix
+++ nix/config.nix
@@ -1,10 +1,29 @@
+# Configuration module for the TMS Server stack
+
 { ... }:
 {
+  # Import the dependency modules that provide options
   imports = [
     ./modules/tms-server.nix
+    ./modules/postgres.nix
   ];
+
+  # TMS Server specific options
+  # (All the options have types and defaults. See ./modules/tms-server.nix)
+  #
   TMS_ROOT_DIR = "/tmp/tms4";
+  # TMS_DB_HOST = "localhost";
+  # TMS_DB_PORT = 4323;
+  # TMS_DB_DB_NAME = "tmsdb";
+  # TMS_DB_USER = "tms";
+  # TMS_DB_USER_PASSWORD = "password";
+  # TMS_SSL_CERT_PATH = "";
+  # TMS_SSL_KEY_PATH = "";
+
+  # Postgres speficic options (for local development)
+  # (See types and defaults in ./modules/postgres.nix)
+  #
   POSTGRES_USER = "postgres";
   POSTGRES_PASSWORD = "foo";
-  TMS_DB_PORT = 4323;
+  POSTGRES_DB = "postgres";
 }
