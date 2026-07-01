207dda86ea8e3b9fab5016b3b5f0a48643d77366
Walter Moreira <walter@waltermoreira.net>
2026-07-01 14:36:09 -0500

    More robust start-up for postgres

--- nix/modules/postgres.nix
+++ nix/modules/postgres.nix
@@ -39,23 +39,33 @@
               podman
               postgresql
               coreutils
+              netcat
             ];
             text = ''
               [[ "$(id -u)" -ne "0" ]] && printf "Please, run \'${name}\` as root\n" && exit 1
               echo "Starting postgres"
-              ${pkgs.podman}/bin/podman image trust set --type accept default
+              podman image trust set --type accept default
               env \
                 PATH="$PATH" \
                 POSTGRES_USER="${config.POSTGRES_USER}" \
                 POSTGRES_PASSWORD="${config.POSTGRES_PASSWORD}" \
                 POSTGRES_DB="${config.POSTGRES_DB}" \
                 TMS_DB_PORT="${toString config.TMS_DB_PORT}" \
-                ${pkgs.podman-compose}/bin/podman-compose \
+                podman-compose \
                   -f ${./../../deployment/postgres/tms-postgres.yml} up \
                   --force-recreate -d
-              sleep 1
-              echo "Waiting max 10 seconds for Postgres to start..."
-              ${pkgs.postgresql}/bin/pg_isready -h ${config.TMS_DB_HOST} -p ${toString config.TMS_DB_PORT} \
+              retries=0
+              while ! nc -z "${config.TMS_DB_HOST}" "${toString config.TMS_DB_PORT}"; do 
+                if [ $retries -gt 5 ]; then
+                  echo "postgres did not start"
+                  exit 1
+                fi
+                echo "no connection, trying again..."
+                sleep 1 
+                retries=$((retries+1))
+              done
+              echo "Checking postgres connection..."
+              pg_isready -h ${config.TMS_DB_HOST} -p ${toString config.TMS_DB_PORT} \
                 -U ${config.POSTGRES_USER} -t 10
             '';
           };
@@ -66,12 +76,20 @@
                 -U ${config.POSTGRES_USER} "$@"
             '';
           };
+          pgIsReady = pkgs.writeShellApplication {
+            name = "pg_isready";
+            text = ''
+              ${pkgs.postgresql}/bin/pg_isready -h ${config.TMS_DB_HOST} -p ${toString config.TMS_DB_PORT} \
+                -U ${config.POSTGRES_USER} "$@"
+            '';
+          };
           postgres = pkgs.symlinkJoin {
             name = "postgres";
             paths = [
               postgresUp
               postgresDown
               psql
+              pgIsReady
             ];
           };
         in
