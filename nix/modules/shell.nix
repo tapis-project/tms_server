f5c6911b79dbb6e98386ee3eac5e300dde00287b
Walter Moreira <walter@waltermoreira.net>
2026-07-01 14:35:55 -0500

    Add packages to devshell

--- nix/modules/shell.nix
+++ nix/modules/shell.nix
@@ -5,6 +5,15 @@
     {
       devShells.default = (config.craneLib.devShell.override {
         mkShell = inputs'.shell-utils.lib.shell;
-      }) { };
+      }) { 
+        name = "TMS-Server-Dev";
+        extraInitRc = ''
+          alias sudo='\sudo env PATH="$PATH" HOME="$HOME"'
+        '';
+        packages = with config.packages; [
+          tms-server-stack
+          postgres
+        ];
+      };
     };
 }
