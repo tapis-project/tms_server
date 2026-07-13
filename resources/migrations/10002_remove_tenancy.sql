0d107e40dc6a1b060c870f1391fc585e139258a9
Steve Black <scblack@tacc.utexas.edu>
2026-07-13 17:10:54 -0500

    WIP remove-tenancy, sql migration.

--- resources/migrations/10002_remove_tenancy.sql
+++ resources/migrations/10002_remove_tenancy.sql
@@ -17,31 +17,37 @@ ALTER TABLE user_hosts DROP CONSTRAINT user_hosts_tenant_fkey;
 ALTER TABLE user_mfa DROP CONSTRAINT user_mfa_tenant_fkey;
 ALTER TABLE clients DROP CONSTRAINT clients_tenant_fkey;
 
+------------------------------------------------------------------------
+-- Add updated unique constrains.
+-- NOTE: Some of these are needed for the multi-column FK updates.
+------------------------------------------------------------------------
+ALTER TABLE hosts ADD CONSTRAINT hosts_host_addr_key UNIQUE (host, addr);
+ALTER TABLE admin ADD CONSTRAINT admin_admin_user_key UNIQUE (admin_user);
+ALTER TABLE delegations ADD CONSTRAINT delegations_client_id_client_user_id_key UNIQUE (client_id,client_user_id);
+ALTER TABLE user_hosts ADD CONSTRAINT user_hosts_tms_user_id_host_host_account_key UNIQUE (tms_user_id, host, host_account);
+ALTER TABLE user_mfa ADD CONSTRAINT user_mfa_tms_user_id_key UNIQUE (tms_user_id);
+ALTER TABLE clients ADD CONSTRAINT clients_client_id UNIQUE (client_id);
+ALTER TABLE clients ADD CONSTRAINT clients_app_name_app_version_key UNIQUE (app_name,app_version);
+
 ------------------------------------------
 -- Update multi-column FKs
 ------------------------------------------
 ALTER TABLE reservations DROP CONSTRAINT reservations_tenant_client_id_fkey;
 ALTER TABLE reservations ADD FOREIGN KEY(client_id) REFERENCES clients(client_id) ON UPDATE CASCADE ON DELETE CASCADE;
-
 ALTER TABLE reservations DROP CONSTRAINT reservations_tenant_client_user_id_fkey;
 ALTER TABLE reservations ADD FOREIGN KEY(client_user_id) REFERENCES user_mfa(tms_user_id) ON UPDATE CASCADE ON DELETE CASCADE;
 
 ALTER TABLE pubkeys DROP CONSTRAINT pubkeys_tenant_client_user_id_host_host_account_fkey;
 ALTER TABLE pubkeys ADD FOREIGN KEY(client_user_id, host, host_account) REFERENCES user_hosts(tms_user_id, host, host_account) ON UPDATE CASCADE ON DELETE CASCADE;
-
-
 ALTER TABLE pubkeys DROP CONSTRAINT pubkeys_tenant_client_user_id_fkey;
 ALTER TABLE pubkeys ADD FOREIGN KEY(client_user_id) REFERENCES user_mfa(tms_user_id) ON UPDATE CASCADE ON DELETE CASCADE;
-
 ALTER TABLE pubkeys DROP CONSTRAINT pubkeys_tenant_client_id_fkey;
 ALTER TABLE pubkeys ADD FOREIGN KEY(client_id) REFERENCES clients(client_id) ON UPDATE CASCADE ON DELETE CASCADE;
-
 ALTER TABLE pubkeys DROP CONSTRAINT pubkeys_tenant_client_id_client_user_id_fkey;
 ALTER TABLE pubkeys ADD FOREIGN KEY(client_id, client_user_id) REFERENCES delegations(client_id, client_user_id) ON UPDATE CASCADE ON DELETE CASCADE;
 
 ALTER TABLE delegations DROP CONSTRAINT delegations_tenant_client_id_fkey;
 ALTER TABLE delegations ADD FOREIGN KEY(client_id) REFERENCES clients(client_id) ON UPDATE CASCADE ON DELETE CASCADE;
-
 ALTER TABLE delegations DROP CONSTRAINT delegations_tenant_client_user_id_fkey;
 ALTER TABLE delegations ADD FOREIGN KEY(client_user_id) REFERENCES user_mfa(tms_user_id) ON UPDATE CASCADE ON DELETE CASCADE;
 
@@ -49,32 +55,20 @@ ALTER TABLE user_hosts DROP CONSTRAINT user_hosts_tenant_tms_user_id_fkey;
 ALTER TABLE user_hosts ADD FOREIGN KEY(tms_user_id) REFERENCES user_mfa(tms_user_id) ON UPDATE CASCADE ON DELETE CASCADE;
 
 ------------------------------------------
--- Update unique constraints
+-- Remove old unique constraints
 ------------------------------------------
 ALTER TABLE hosts DROP CONSTRAINT hosts_tenant_host_addr_key;
-ALTER TABLE hosts ADD CONSTRAINT hosts_host_addr_key UNIQUE (host, addr);
-
 ALTER TABLE admin DROP CONSTRAINT admin_tenant_admin_user_key;
-ALTER TABLE admin ADD CONSTRAINT admin_admin_user_key UNIQUE (admin_user);
-
 ALTER TABLE delegations DROP CONSTRAINT delegations_tenant_client_id_client_user_id_key;
-ALTER TABLE delegations ADD CONSTRAINT delegations_client_id_client_user_id_key UNIQUE (client_id,client_user_id);
-
 ALTER TABLE user_hosts DROP CONSTRAINT user_hosts_tenant_tms_user_id_host_host_account_key;
-ALTER TABLE user_hosts ADD CONSTRAINT user_hosts_tms_user_id_host_host_account_key UNIQUE (tms_user_id,host,host_account);
-
 ALTER TABLE user_mfa DROP CONSTRAINT user_mfa_tenant_tms_user_id_key;
-ALTER TABLE user_mfa ADD CONSTRAINT  user_mfa_tms_user_id_key UNIQUE (tms_user_id);
-
 ALTER TABLE clients DROP CONSTRAINT clients_tenant_app_name_app_version_key;
-ALTER TABLE clients ADD CONSTRAINT clients_app_name_app_version_key UNIQUE (app_name,app_version);
 
 ------------------------------------------
 -- Update indexes
 ------------------------------------------
 DROP INDEX CONCURRENTLY IF EXISTS delg_user_client_idx;
 CREATE UNIQUE INDEX IF NOT EXISTS delg_user_client_idx ON delegations (client_id, client_user_id);
-
 DROP INDEX CONCURRENTLY IF EXISTS clients_tenant_client_idx;
 CREATE UNIQUE INDEX IF NOT EXISTS clients_tenant_client_idx ON clients (client_id);
 
@@ -88,6 +82,7 @@ ALTER TABLE pubkeys DROP COLUMN tenant;
 ALTER TABLE delegations DROP COLUMN tenant;
 ALTER TABLE user_hosts DROP COLUMN tenant;
 ALTER TABLE user_mfa DROP COLUMN tenant;
+ALTER TABLE clients DROP COLUMN tenant;
 
 ------------------------------------------
 -- Last action is to drop the tenants table
