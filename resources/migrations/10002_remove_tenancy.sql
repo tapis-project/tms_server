11471cc9d9dd8f3acc668e7a65db3a435177199e
Steve Black <scblack@tacc.utexas.edu>
2026-07-13 08:53:56 -0500

    WIP remove-tenancy

---
+++ resources/migrations/10002_remove_tenancy.sql
@@ -0,0 +1,14 @@
+--
+-- Remove tenants table and related columns
+--
+
+-- Remove all foreign key relations for tenants table
+
+-- Remove tenant column from tables
+
+
+-- ---------------------------------------
+-- Last action is to drop the tenants table ?
+-- ---------------------------------------
+
+DROP TABLE IF EXISTS tenants cascade;
